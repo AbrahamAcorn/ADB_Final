41616dd702bc5b0680cf0585d3297e31048513b5
Abraham <abrahmr0@gmail.com>
2020-05-30 21:48:26 -0500

    Se mudo el proyecto a maven

---
+++ src/main/java/modelo/Citas.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Abraham
+ */
+public class Citas {
+    private int paciente;
+    private int doctor;
+    private String fecha;
+    private String hora;
+
+    public Citas(int paciente, int doctor, String fecha, String hora) {
+        this.paciente = paciente;
+        this.doctor = doctor;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public Citas() {
+    }
+
+    public int getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(int paciente) {
+        this.paciente = paciente;
+    }
+
+    public int getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(int doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    
+    
+}
